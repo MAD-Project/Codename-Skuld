d9f76b2011047b3fa430bed04a889d9a4c05fa52
Alexsandro <alexsandro.ruiz@ikasle.egibide.org>
2018-11-09 14:06:51 +0100

    login de usuario y cambios en index.php

--- Web/index.php
+++ Web/index.php
@@ -8,8 +8,19 @@
         <title>Codename Skuld</title>
         <link rel="stylesheet" href="./css/style.css" />
 
+        <!-- css para el login -->
+        <link rel="stylesheet" type="text/css" href="./css/login.css">
+        <!-- css de iconos -->
+        <link rel="stylesheet" href="./css/fonts.css">
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Montez|Pathway+Gothic+One" rel="stylesheet">
+
         <!-- CSS externos -->
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+        <?php session_start(); ?>
+
+        <?php include 'paginasPhp/login.php' ?> <!-- login -->
+
     </head>
 
     <body class="grid-container">
@@ -75,7 +86,18 @@
             </div>
         </div>
         <div class="sidebar">
-            sidebar
+            <div class="login">
+                <article class="fondo">
+                    <h3>Inicio de Sesión</h3>
+                        <form id="login" name="flogin" method="post">
+                            <span class="icon-user"></span><input class="inp" type="email" id="emailLogin" name="emailLogin" required><br>
+                            <span class="icon-key"></span><input class="inp" type="password" id="passwordLogin" name="passwordLogin" required"><br>
+                            <input class="boton" type="submit" value="Login">
+                        </form>
+                </article>
+                <a href="paginas/paginaRegistro.php"><p>Crear una cuenta</p></a>
+            </div>           
+
         </div>
         <footer class="footer">
             footer
