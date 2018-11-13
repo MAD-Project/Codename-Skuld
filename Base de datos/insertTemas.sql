2db11dc64b3db52fde24e491a505e0b9772b123d
Mikel <mklferreiro@gmail.com>
2018-11-13 12:00:22 +0100

    InsertBase + creacion de tablas

--- Base de datos/insertTemas.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO usuario (nombreUsuario,password,correo) VALUES ("Mikel","123","mikel@gmail.com");
-INSERT INTO usuario (nombreUsuario,password,correo) VALUES ("Dani","123","dani@gmail.com");
-INSERT INTO usuario (nombreUsuario,password,correo) VALUES ("Alex","123","alex@gmail.com");
-
-
-INSERT INTO valoracion(idUsuario, idTema) VALUES(2,)
-INSERT INTO tema(idUsuario, titulo, texto, valoracion, mejorRespuesta, fechaTema)
-VALUES (2,"Titular titulastico","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",0,null,"24/10/2018");
