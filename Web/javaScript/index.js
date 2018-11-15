e2a57e0f996991a15a4f30316931abf619024d5d
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2018-11-15 10:16:02 +0100

    Cambios

--- Web/javaScript/index.js
+++ Web/javaScript/index.js
@@ -1,5 +1,5 @@
-function mostrarLogin() {
-    document.getElementById("login").classList.add("abrirlogin");
+function mostrarBox(box) {
+    document.getElementById(box).classList.add("abrirlogin");
     document.getElementById("sidebar").classList.add("abrirlogin");
     document.getElementById("main").style.display = "none";
 }
