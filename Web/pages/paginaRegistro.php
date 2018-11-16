52162662449114e479fc3048308a6ee10b88c9ae
Daniel Barragues <25356150+Barraguesh@users.noreply.github.com>
2018-11-16 10:36:12 +0100

    Registro estilado y pequeños retoques
    La vista en Ipad tiene barra de búsqueda.

--- Web/pages/paginaRegistro.php
+++ Web/pages/paginaRegistro.php
@@ -9,26 +9,25 @@
     </head>
 
     <body>
-
         <form class="registro" id="registro" name="fregistro" method="post" action="javascript:void(0)">
 
             <label for="nombre">Nombre</label>
-            <input class="inputRegistro" type="text" id="nombre" name="nombre" placeholder="Nombre" autofocus />
+            <input class="inputRegistro" type="text" id="nombre" name="nombre" autofocus />
             <label for="apellido">Apellido</label>
-            <input class="inputRegistro" type="text" id="apellido" name="apellido" placeholder="Apellido" />
+            <input class="inputRegistro" type="text" id="apellido" name="apellido" />
             <label for="nombreU">Nombre Usuario *</label>
-            <input type="text" id="nombreU" name="nombreU" class="require inputRegistro" placeholder="Nombre de usuario" />
+            <input type="text" id="nombreU" name="nombreU" class="require inputRegistro" />
             <label for="email">Correo electrónico *</label>
             <input type="email" id="email" name="email" class="require inputRegistro" placeholder="ejemplo@gmail.com" />
-            <label for="password">Password *</label>
-            <input type="password" id="password" name="password" class="require inputRegistro" placeholder="Contraseña" />
+            <label for="password">Contraseña *</label>
+            <input type="password" id="password" name="password" class="require inputRegistro" />
 
 
             <input class="registroBTN" type="submit" onclick="eviarDatos('../Web/controller/registro.php','registro','post')"
                 value="Registarse" />
+            <a onclick="ocultarElementosSidebar('registro','box')">Volver</a>
 
         </form>
-
         <p id="resultado"></p>
 
     </body>
