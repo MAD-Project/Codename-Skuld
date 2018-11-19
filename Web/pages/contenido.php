a028093838160b69831eb8d1ecbdbd519549595d
Mikel <mklferreiro@gmail.com>
2018-11-19 13:20:09 +0100

    votar disabled no login

--- Web/pages/contenido.php
+++ Web/pages/contenido.php
@@ -3,23 +3,19 @@
         include_once 'controller/consultasBD.php';
         include_once 'controller/busquedas.php';
 
-
         if (isset($_POST['search'])){
-
             $temas = verDatosBusqueda();
-
         }
         else{
-
             $temas = cargarTemas();
-
         }
 
         foreach ($temas as $tema):?>
     <div class="temaBox" id=<?= $tema["id"] ?>>
         <div>
             <p class="votos" id=<?="puntuacion",$tema['id'] ?>> <?= $tema["valoracion"] ?></p>
-            <input type="button" value="Votar" class="votarBTN" onclick="votarPuntuacion('<?=$tema['id']?>')" id=<?="votar",$tema['id'] ?>>
+            <input type="button" value="Votar" class="votarBTN" onclick="votarPuntuacion('<?=$tema['id']?>')" id="<?="votar",$tema['id'] ?>"
+                   <?= !(isset($_SESSION['login']) && $_SESSION['login']===true)?'disabled':''; ?>>
         </div>
         <div onclick="alert('link')">
             <h2>
