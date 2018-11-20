4f0442b05270e7ffa1af8743217ed2eb68b454b0
Mikel <mklferreiro@gmail.com>
2018-11-20 14:08:11 +0100

    etiquetas Empezando

---
+++ Web/pages/buscador.php
@@ -0,0 +1,19 @@
+
+<form id="buscador" name="buscador" method="POST" action="javascript:void(0)">
+    <input class="search" id="search" name="search" type="search" list="listaEtiquetas"
+           placeholder="Buscar aquí..." aria-label="Buscar contenido del sitio" autofocus>
+</form>
+
+<datalist id="listaEtiquetas">
+
+    <option value="#Duda">Sugerencia temas</option>
+
+    <option value="#Sugerencia">
+
+    <option value="#Problema">
+
+    <option value="#Urgente">
+
+    <option value="#Solucionado">
+
+</datalist>
